c5480c93ed8f59051072b13d71bf9cf4e5d023fc
ZAYNUL HUDI <zaynulhudi@gmail.com>
2020-07-07 14:16:43 +0700

    Update newsApi.js

--- newsApi.js
+++ newsApi.js
@@ -1,4 +1,4 @@
-const apiKey = '7f65a98cdbf1496088f8cb61e23fd2e8';
+const apiKey = '752d6407bccb43e6956404716fba15c0';
 const topHeadlinesUrl =
   'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
 
